42b8e562aee662971a44667b73ed2077d6914f18
Cobboldgit <86422221+Cobboldgit@users.noreply.github.com>
2021-10-02 10:26:50 +0000

    add initials

--- js-practice/react-projects/react-news-app/src/App.js
+++ js-practice/react-projects/react-news-app/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import News from "./pages/News";
+import HeroSection from "./component/HeroSection";
+import Footer from './component/Footer'
+import {Spinner} from 'react-bootstrap'
 
 function App() {
+  const [article, setArticle] = useState([]);
+  const [term, setTerm] = useState("world");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getArticle = async () => {
+    fetch(
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${term}&api-key=XtFEnROqyLO1tOboNj4geVFBMLcjzPbU`
+    )
+      .then((response) => response.json())
+      .then((articleData) => {
+        setArticle(articleData.response.docs);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getArticle();
+    return setArticle([]);
+  }, [term]);
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeroSection term={term} searchText={(text)=> setTerm(text)}/>
+      {isLoading ? (
+        <div className="isLoading"><Spinner animation="border" variant="primary" size="lg" /></div>
+      ) : (
+        article.map((articles, index) => {
+          return (
+            <div key={index}>
+              <News
+                article={articles}
+                key={articles._id}
+                isLoading={isLoading}
+              />
+            </div>
+          );
+        })
+      )}
+      <Footer/>
     </div>
   );
 }
